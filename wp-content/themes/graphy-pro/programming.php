ca3d39b21656a8a470d855acda89287537ec3eb9
HK Kahng <hk@router-rooter.com>
2017-07-16 18:19:54 +0000

    add some error catching from the api returns on the show schedule template

--- wp-content/themes/graphy-pro/programming.php
+++ wp-content/themes/graphy-pro/programming.php
@@ -19,63 +19,63 @@ get_header(); ?>
     $request = wp_remote_get( 'http://admin.freeformportland.org/api/v1/shows?startWeek=' . strtolower(date('l')));
 
     if( is_wp_error( $request ) ) {
-    	return false;
-    }
+      echo '<h1>Whoops, something is not working correctly! Hold on, we are looking into it!</h1>';
+    } else {
     
     $body = wp_remote_retrieve_body( $request );
 
     $data = json_decode( $body, true );
     
-    #var_dump($data);
-    
     $loop_day = '';
     $loop_hour = '';
     
+    if( ! empty( $data ) && empty($data['statusCode']) ) {
+      foreach( $data as $show ) {
+  
+        $day = $show['dayOfWeek'];
+  
+        if ($day !== $loop_day ) {
+          if ($loop_day !== '') {
+            echo '</ul></section>';
+          }
+  
+          $loop_day = $day;
+  
+          if ($day < 7) {
+          ?>
+          <section class="schedule-block">
+            <div class="day">
+            <?= $day; ?>
+            </div>
+  
+            <ul class="schedule">
+          <?php
+          }
+        }
+  
+        $hour = $show['startTime'];
+  
+        if ($day < 7 && $hour !== '') {
+          if ($hour !== $loop_hour) {
+            $loop_hour = $hour;
+          ?>
+          <li class="hour-<?= $hour ?>">
+            <?php } ?>
+            <a href="/program/?show_name=<?php echo $show['slug'] ?>">
+            <?= convert_to_twelve($hour) ?> -
+            <?php echo $show['showName'] . ' w/ ' . show_all_djs($show['users']); ?>
+            </a>
+          <?php if ($hour !== $loop_hour) { ?>
+          <li class="hour-<?= $hour ?>">
+          </li>
+          <?php } else { echo("<br />"); }
+          }
+        };
+      } else {
+        echo '<h1>Whoops, something is not working correctly! Hold on, we are looking into it!</h1>';
+      };
+    }; ?>
     
-    if( ! empty( $data ) ) : foreach( $data as $show ) {
-
-      $day = $show['dayOfWeek'];
-
-      if ($day !== $loop_day ) {
-        if ($loop_day !== '') {
-          echo '</ul></section>';
-        }
-
-        $loop_day = $day;
-
-        if ($day < 7) {
-        ?>
-        <section class="schedule-block">
-          <div class="day">
-          <?= $day; ?>
-          </div>
-
-          <ul class="schedule">
-        <?php
-        }
-      }
-
-      $hour = $show['startTime'];
-
-      if ($day < 7 && $hour !== '') {
-        if ($hour !== $loop_hour) {
-          $loop_hour = $hour;
-        ?>
-        <li class="hour-<?= $hour ?>">
-        <?php } ?>
-
-          <a href="/program/?show_name=<?php echo $show['slug'] ?>">
-          <?= convert_to_twelve($hour) ?> -
-          <?php echo $show['showName'] . ' w/ ' . show_all_djs($show['users']); ?>
-          </a>
-        <?php if ($hour !== $loop_hour) { ?>
-        <li class="hour-<?= $hour ?>">
-        </li>
-        <?php } else { echo("<br />"); }
-        } ?>
-    <?php
-    }; endif;
-    ?>
     </ul>
     </section>
 
