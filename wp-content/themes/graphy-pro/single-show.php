50dbcee0556ff524812d1585f0782122138752cc
HK Kahng <hk@router-rooter.com>
2017-07-29 05:07:53 +0000

    consume and display Pollard API via PHP

--- wp-content/themes/graphy-pro/single-show.php
+++ wp-content/themes/graphy-pro/single-show.php
@@ -45,6 +45,17 @@ get_header(); ?>
 
 <?php
 $showID = $custom_fields['id'][0];
+
+$request = wp_remote_get( 'http://kffp.rocks/api/setlistsByShowID/' . $showID );
+
+if( is_wp_error( $request ) ) {
+	return false;
+}
+
+$body = wp_remote_retrieve_body( $request );
+
+$data = json_decode( $body, true );
+
 ?>
 
 <? // if dj is logged in, show the show id and a link to create a playlist ?>
@@ -59,93 +70,35 @@ $showID = $custom_fields['id'][0];
 
 <?php } ?>
 
-<ul class="playlist-list"></ul>
-
-<script>
-if (typeof jQuery !== 'undefined') {
-  (function($) {
-    function formatDate(date) {
-      var monthNames = [
-        "January", "February", "March",
-        "April", "May", "June", "July",
-        "August", "September", "October",
-        "November", "December"
-      ];
-
-      var pieces = date.split('-'),
-          output = monthNames[parseInt(pieces[1]) - 1] + ' ' + parseInt(pieces[2]) + ', ' + pieces[0];
-
-      return output;
-    }
-
-    window.formatDate = formatDate;
-
-    function fetchPlaylistInfo(showID) {
-      var $playlistList = $('<ul>');
-
-      $.ajax({
-        url: 'http://kffp.rocks/api/setlistsByShowID/' + showID,
-        dataType: 'json',
-        success: function(data) {
-          if (!data.length) {
-            $playlistList.append("<li class='loading'>This DJ hasn't created any playlists</li>");
-            clearInterval(playlistFetchingInterval);
+<?php
+  echo '<ul class="playlist-list">';
+  if ( count($data) > 0 ) {
+    foreach ( array_reverse($data) as $episode ) {
+      if ( count( $episode['songs'] ) > 0) {
+        $playlistDate = date_create($episode['createdAt'])->setTimezone(new DateTimeZone('America/Los_Angeles'));
+        echo '<li><h3>' . date_format($playlistDate, "F d, Y") . '</h3>';
+        echo '<ul class="playlist-songs">';
+        foreach($episode['songs'] as $song) {
+          if ( $song['inputs'][1]['value'] != 'freeformportland.org') {
+            echo '<li>';
+            echo '<span class="song-timestamp" style="display: none;">' . $song['playedAt'] . '</span>';
+            echo '<span class="song-artist">' . ($song['inputs'][1]['value'] ? $song['inputs'][1]['value'] : '' ) . '</span>';
+            echo ' - ';
+            echo '<span class="song-title">' . ($song['inputs'][0]['value'] ? $song['inputs'][0]['value'] : '' ) . '</span>';
+            echo '<span class="song-album" style="display: none;">' . ($song['inputs'][2]['value'] ? $song['inputs'][2]['value'] : '' ) . '</span>';
+            echo '<span class="song-label" style="display: none;">' . ($song['inputs'][3]['value'] ? $song['inputs'][3]['value'] : '' ) . '</span>';
+            echo '</li>';
           }
-
-          for (var i = data.length - 1; i >= 0; i--) {
-            var playlist = data[i],
-            played = false,
-            playlistDate = formatDate(playlist.createdAt.split('T')[0]),
-            $thisPlaylist = $('<li>');
-
-            $thisPlaylist.append('<h3>' + playlistDate + '</h3>');
-
-            var $songs = $('<ul class="playlist-songs">').appendTo($thisPlaylist);
-
-            $(playlist.songs).each(function(i, song) {
-              if (song.played && song.inputs[0].value != 'KFFP 90.3') {
-                played = true;
-
-                var songTimeStamp = new Date(Date.parse(song.playedAt));
-
-                var songHTML = '<li>';
-                songHTML += '<span class="song-timestamp" style="display:none;">' + songTimeStamp + '</span>';
-                songHTML += '<span class="song-artist">' + song.inputs[1].value + '</span>';
-                songHTML += ' - ';
-                songHTML += '<span class="song-title">' + song.inputs[0].value + '</span>';
-                songHTML += '<span class="song-album" style="display:none;">' + song.inputs[2].value + '</span>';
-                songHTML += '<span class="song-label" style="display:none;">' + song.inputs[3].value + '</span>';
-                $songs.prepend(songHTML);
-              }
-            });
-
-            if (played) {
-              $thisPlaylist.appendTo($playlistList);
-            }
-          }
-
-          $('.playlist-list').html( $playlistList.html() );
-
-          if (typeof createPlaylistLinkList == 'function') {
-            createPlaylistLinkList(data);
-          }
-        },
-        error: function(jqXHR, textStatus, errorThrow) {
-          $playlistList.empty().append('<li>Error loading playlists</li>');
         }
-      });
+        echo '</ul>';
+        echo '</li>';
+      }
     }
-
-    var showID = '<?= $showID ?>';
-
-    fetchPlaylistInfo(showID);
-
-    var playlistFetchingInterval = setInterval(fetchPlaylistInfo, 60000, showID);
-
-
-  })(jQuery);
-}
-</script>
+  } else {
+    echo '<h3>No playlists found</h3>';
+  }
+  echo '</ul>';
+?>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
