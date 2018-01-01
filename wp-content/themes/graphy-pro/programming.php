03720b220bed3b1e1608628594044b43a87e585d
HK Kahng <hk@router-rooter.com>
2018-01-01 12:19:58 -0800

    update API endpoint references to use HTTPS

--- wp-content/themes/graphy-pro/programming.php
+++ wp-content/themes/graphy-pro/programming.php
@@ -15,49 +15,49 @@ get_header(); ?>
     </header>
 
     <?php
-    
+
     $today = new DateTime();
     $today_pacific = $today->setTimezone(new DateTimeZone('America/Los_Angeles'))->format('l');
-    
-    $request = wp_remote_get( 'http://admin.freeformportland.org/api/v1/shows?isActive=true&startWeek=' . strtolower($today_pacific) );
+
+    $request = wp_remote_get( 'https://admin.freeformportland.org/api/v1/shows?isActive=true&startWeek=' . strtolower($today_pacific) );
 
     if( is_wp_error( $request ) ) {
       echo '<h1>Whoops, something is not working correctly! Hold on, we are looking into it!</h1>';
     } else {
-    
+
     $body = wp_remote_retrieve_body( $request );
 
     $data = json_decode( $body, true );
-    
+
     $loop_day = '';
     $loop_hour = '';
-    
+
     if( ! empty( $data ) && empty($data['statusCode']) ) {
       foreach( $data as $show ) {
-  
+
         $day = $show['dayOfWeek'];
-  
+
         if ($day !== $loop_day ) {
           if ($loop_day !== '') {
             echo '</ul></section>';
           }
-  
+
           $loop_day = $day;
-  
+
           if ($day < 7) {
           ?>
           <section class="schedule-block">
             <div class="day">
             <?= $day; ?>
             </div>
-  
+
             <ul class="schedule">
           <?php
           }
         }
-  
+
         $hour = $show['startTime'];
-  
+
         if ($day < 7 && $hour !== '') {
           if ($hour !== $loop_hour) {
             $loop_hour = $hour;
@@ -78,7 +78,7 @@ get_header(); ?>
         echo '<h1>Whoops, something is not working correctly! Hold on, we are looking into it!</h1>';
       };
     }; ?>
-    
+
     </ul>
     </section>
 
